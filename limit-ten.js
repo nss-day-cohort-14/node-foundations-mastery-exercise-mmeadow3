6f80d6d5b3e83aab4ec256374c4d2170706612db
Mike Meadows <mmeadow33@gmail.com>
2016-08-26 15:33:40 -0500

    more efficient word search

--- limit-ten.js
+++ limit-ten.js
@@ -5,20 +5,10 @@ const { Transform } = require("stream");
 
 const transformer = Transform()
 transformer._transform = (buff, enc, cb) => {
-  cb(null, buff.toString().toUpperCase())
+
+
+	
+  cb(null, buff.toString())
 }
 
 module.exports = transformer
-
-
-// const transformer = Transform()
-// transformer._transform = function (buff, enc, cb) {
-//  cb (null, function () { 
-//  	if (`${buff.toString().includes("z")}`){
-//  console.log(`${buff.toString()}`)
-//  	} else {
-//  		console.log("words not found")
-//  	}
-// })
-// }
-// module.exports = transformer
--- word-search.js
+++ word-search.js
@@ -5,41 +5,19 @@ const limit = require("./limit-ten")
 const transformStream = Transform()
 
 
-function arg(){
-	return process.argv[2].toString()
-}
 
-let file = fs.createReadStream('/usr/share/dict/words', {flags: 'r'})
-  .pipe(es.split(""))
+let file = fs.createReadStream('/usr/share/dict/words')
+  .pipe(es.split())
   .pipe(es.map(transformStream._transform = (buffer, callback) => {
 	 if (buffer.toString().startsWith((process.argv[2].toString()).toLowerCase())) {
-	 	let rightWord = (buffer.toString().match(process.argv[2].toString()))
-	 	console.log(rightWord.input)
-  		callback(null, buffer.toString())
-	 	// } else {
-	 	// 	console.log("word not found")
-	 	// callback()	
-}
-  }))
+	 	let rightWord = (buffer.toString())
+  		callback(null, buffer.toString() + '\n')
+		} 
+		callback()
+  	})	
+) 
 
-
-// 	transformStream._transform = (buffer, encoding, cb) => {
-// 	 if (buffer.toString().startsWith(("app").toLowerCase())) {
-// 	 	let rightWord = (buffer.toString().match("app"))
-// 	 	console.log(rightWord)
-// 	 	} else {
-//   		console.log("Word Not Found");	
-// }
-// 	  cb(null, buffer)
-// } 
-
-
-
-
-
-
-
-.pipe(es.map(limit))
+.pipe(limit)
 .pipe(process.stdout)
 
 
