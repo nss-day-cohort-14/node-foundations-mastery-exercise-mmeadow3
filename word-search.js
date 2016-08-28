6d9ec13b7901abd3b237177836dadae4c972db92
Mike Meadows <mmeadow33@gmail.com>
2016-08-28 16:33:03 -0500

    re-fixed case senstivity issue

--- word-search.js
+++ word-search.js
@@ -8,17 +8,14 @@ const limit = require("./limit-ten")
 let file = fs.createReadStream('/usr/share/dict/words') ////starts reading file
   .pipe(es.split())///////Splits all the data into single lines
   .pipe(es.map((buffer, callback) => {
-	 if (buffer.toString().startsWith((process.argv[2].toString()))) {
+	 if (buffer.toString().toLowerCase().startsWith((process.argv[2].toString()))) {
   		callback(null, buffer.toString() + '\n')
 		} else {
 		callback()
 	}
-  	})	
-) 
+  	})
+)
 /////pipe to limit-ten.js
 .pipe(limit)
 /////pipe to stdout/////
 .pipe(process.stdout)
-
-
-
